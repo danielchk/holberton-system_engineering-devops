be5de81f31b58fcbcdb546938bdcee80365d904d
danielchk <hollowdaniel1@gmail.com>
2020-06-22 22:50:53 +0000

    I see dead processes

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * infinite_while - creates an infinite loop to make the program hang
+ * Return: always 0
+ */
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+
+/**
+ * main - 5 zombies
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+	pid_t zom;
+
+	for (i = 0; i < 5; i++)
+	{
+		zom = fork();
+		if (!zom)
+			return (0);
+		printf("Zombie process created, PID: %d\n", zom);
+	}
+
+	infinite_while();
+	return (0);
+}

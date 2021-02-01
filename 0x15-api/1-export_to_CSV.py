d8f4f5fafab91ecb19d4a231437a06084018341a
danielchk <hollowdaniel1@gmail.com>
2021-02-01 16:21:44 -0500

    fixing 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -6,15 +6,15 @@ import csv
 import requests
 from sys import argv
 
-if __name__ == '__main__':
-    api = argv[1]
-    user = requests.get("https://jsonplaceholder.typicode.com/users/{}".
-                        format(api), verify=False).json()
-    todo = requests.get("https://jsonplaceholder.typicode.com/todos?api={}".
-                        format(api), verify=False).json()
-    with open("{}.csv".format(api), 'w', newline='') as csvfile:
-        taskwriter = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        for task in todo:
-            taskwriter.writerow([int(api), user.get('username'),
-                                 task.get('completed'),
-                                 task.get('title')])
+if __name__ == "__main__":
+    userid = argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(userid)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": userid}).json()
+
+    with open("{}.csv".format(userid), "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        [writer.writerow(
+            [userid, username, t.get("completed"), t.get("title")]
+         ) for t in todos]
